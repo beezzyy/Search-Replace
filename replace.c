2ae74047e96905cd0e77bd1f53be7ec9621690fa
Adir Ben-David <91300383+beezzyy@users.noreply.github.com>
2022-10-14 17:06:25 -0400

    Update Code

--- replace.c
+++ replace.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <string.h>
+#include <stdlib.h>
 #include "text.h"
 #include "traversal.h"
 #include "report.h"
 
 int main(int argc, char **argv)
 {
+    if (argc < 2)
+    {
+        printf("Argument not found. Please try again\n");
+        exit(0);
+    }
+
     char arg[strlen(argv[1])];
     strcpy(arg, argv[1]);
     char *dirname = ".";
